d76229730dd0fe3d05974859860bafebaba3220e
Maor Shushan <mshushan45@gmail.com>
2022-01-24 18:09:00 +0200

    background component

---
+++ study-hub/src/components/Background.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="background-container">
+        <v-img
+          :src="require('../assets/background.jpg')"
+          id="background-img"
+        />
+        <slot id="page-content"></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Background'
+  }
+</script>
+
+<style scoped>
+#background-container {
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+    position: relative;
+}
+
+#background-img {
+    position: absolute;
+    left: 0;
+    top: 0;
+    min-width: 100vw;
+    min-height: 100vh;
+}
+
+#background-img::after {
+    content: "";
+    background-color: black;
+    position:absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    opacity: .4;
+}
+</style>
