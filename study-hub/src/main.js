9fb48f343185930a7f053b13cececd868264f9cb
Maor Shushan <mshushan45@gmail.com>
2022-01-24 16:26:05 +0200

    fresh project

--- study-hub/src/main.js
+++ study-hub/src/main.js
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
+import { DraggablePlugin } from '@braks/revue-draggable';
 
-createApp(App).use(router).mount('#app')
+Vue.config.productionTip = false
+Vue.use(DraggablePlugin)
+
+new Vue({
+  router,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
+
+
--- study-hub/src/views/Home.vue
+++ study-hub/src/views/Home.vue
@@ -1,34 +1,15 @@
 <template>
-  <div class="home">
-    <v-bottom-navigation v-model="value">
-    <v-btn value="recent">
-      <span>Recent</span>
-
-      <i class="fas fa-address-card"></i>
-    </v-btn>
-
-    <v-btn value="favorites">
-      <span>Favorites</span>
-
-      <v-icon>fa-address-card</v-icon>
-    </v-btn>
-
-    <v-btn value="nearby">
-      <span>Nearby</span>
-
-      <v-icon>mdi-map-marker</v-icon>
-    </v-btn>
-  </v-bottom-navigation>
-  </div>
+  <hello-world />
 </template>
 
 <script>
-// @ is an alias to /src
+  import HelloWorld from '../components/HelloWorld'
 
-export default {
-  name: 'Home',
-  components: {
-    
+  export default {
+    name: 'Home',
+
+    components: {
+      HelloWorld,
+    },
   }
-}
 </script>
